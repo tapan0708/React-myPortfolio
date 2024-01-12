eba0253c8c66869af04101dbf472835e47adb902
Tapan vyas <tapanvyas555@gmail.com>
2024-01-12 15:01:00 +0530

    Set custom base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://tapan0708.github.io/React-portfolio/",
+  base: "/React-portfolio/",
   plugins: [react()],
 });
